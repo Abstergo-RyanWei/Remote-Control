da913dee6e297698fae83c4e777304238d8d3efa
Abstergo-RyanWei <2771066110@qq.com>
2014-04-09 20:07:12 +0800

    FlappyBird

---
+++ FlappyBird/FlappyBird.py
@@ -0,0 +1,19 @@
+import win32com.client
+import time
+import serial
+shell=win32com.client.Dispatch("WScript.Shell")
+arduinos=['COM5','COM6','COM7','COM8','COM9','COM10','COM11',
+'COM12','COM13','COM14','COM15','COM16','COM17','COM18','COM19','COM20']
+for device in arduinos:
+    try:
+        print "Connecting...",device
+        arduino = serial.Serial(device, 9600)
+        break
+    except:
+        print "Failed to connect on",device
+
+while True:
+    line=arduino.readline();
+    time.sleep(0.1)
+    if line=='a':
+        shell.sendKeys("{ }")
